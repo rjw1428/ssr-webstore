8144332aafaa7cb2079266856e556b16db97dce4
Ryan Wilk <rjw1428@gmail.com>
2020-11-21 13:49:39 -0500

    cloud function changes

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -1,10 +1,10 @@
 import * as functions from 'firebase-functions';
 import { db } from './config'
-import * as admin from 'firebase-admin';
-import { tmpdir } from 'os';
-import { join, dirname } from 'path'
-import * as sharp from 'sharp';
-import * as fs from 'fs-extra';
+// import * as admin from 'firebase-admin';
+// import { tmpdir } from 'os';
+// import { join, dirname } from 'path'
+// import * as sharp from 'sharp';
+// import * as fs from 'fs-extra';
 
 
 export const test = functions.https.onCall(async (object: any) => {
@@ -18,47 +18,47 @@ export const test = functions.https.onCall(async (object: any) => {
         })
 })
 
-export const resizeImage = functions.storage.object().onFinalize(async (object: any) => {
-    console.log(object)
-    const contentType = object.contentType;
-    const filePath = object.name;
-    const fileName = filePath.split('/').pop();
-    const destBucket = admin.storage().bucket(object.bucket)
-    const bucketDir = dirname(filePath)
-    const workingDir = join(tmpdir(), 'thumbs');
-    const tmpFilePath = join(workingDir, 'source.png')
+// export const resizeImage = functions.storage.object().onFinalize(async (object: any) => {
+//     console.log(object)
+//     const contentType = object.contentType;
+//     const filePath = object.name;
+//     const fileName = filePath.split('/').pop();
+//     const destBucket = admin.storage().bucket(object.bucket)
+//     const bucketDir = dirname(filePath)
+//     const workingDir = join(tmpdir(), 'thumbs');
+//     const tmpFilePath = join(workingDir, 'source.png')
 
-    if (fileName.includes('thumb@')) {
-        console.log("Already Thumbnail")
-        return false;
-    } else if (!contentType.includes('image')) {
-        console.log("Not an Image")
-        return false;
-    } else {
+//     if (fileName.includes('thumb@')) {
+//         console.log("Already Thumbnail")
+//         return false;
+//     } else if (!contentType.includes('image')) {
+//         console.log("Not an Image")
+//         return false;
+//     } else {
 
-        await fs.ensureDir(workingDir)
+//         await fs.ensureDir(workingDir)
 
-        await destBucket.file(filePath).download({
-            destination: tmpFilePath
-        })
+//         await destBucket.file(filePath).download({
+//             destination: tmpFilePath
+//         })
 
-        const sizes = [100, 200];
+//         const sizes = [100, 200];
 
-        const uploadPromises = sizes.map(async size => {
-            const thumbName = `thumb@${size}_${fileName}`
-            const thumbPath = join(workingDir, thumbName);
+//         const uploadPromises = sizes.map(async size => {
+//             const thumbName = `thumb@${size}_${fileName}`
+//             const thumbPath = join(workingDir, thumbName);
 
-            await sharp(tmpFilePath)
-                .resize(size, size)
-                .toFile(thumbPath)
+//             await sharp(tmpFilePath)
+//                 .resize(size, size)
+//                 .toFile(thumbPath)
 
-            return destBucket.upload(thumbPath, {
-                destination: join(bucketDir, thumbName)
-            })
-        })
+//             return destBucket.upload(thumbPath, {
+//                 destination: join(bucketDir, thumbName)
+//             })
+//         })
 
-        await Promise.all(uploadPromises)
+//         await Promise.all(uploadPromises)
 
-        return fs.remove(workingDir);
-    }
-})
+//         return fs.remove(workingDir);
+//     }
+// })
